0f40092f2900a81d4c545bb8e6f6de4c9e34cbbf
Muhammad Hassaan Moazan <boxloid0321321@gmail.com>
2024-07-27 20:28:23 +0500

    Transition added for each level

--- Assets/Scripts/BananaControl.cs
+++ Assets/Scripts/BananaControl.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BananaControl : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.CompareTag("Player")) {
+    public float delay = 3.0f;
+
+    public Animator animatorF;     // The Animator component
+    public string animationNameF;  // The name of the animation to play
+    public Animator animatorSF;
+    public string animationNameSF;
+    public Animator animatorBG;
+    public string animationNameBG;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animatorF.Play(animationNameF);
+            animatorSF.Play(animationNameSF);
+            animatorBG.Play(animationNameBG);
             EndGame();
         }
     }
 
-    private void EndGame() {
+    private void EndGame()
+    {
+        StartCoroutine(LoadLevelAfterDelay());
+    }
+    private IEnumerator LoadLevelAfterDelay()
+    {
         SceneController sceneController = FindObjectOfType<SceneController>();
-        if (sceneController != null) {
+        yield return new WaitForSeconds(delay); //delay
+        if (sceneController != null)
+        {
             sceneController.LoadNextLevel();
         }
     }
