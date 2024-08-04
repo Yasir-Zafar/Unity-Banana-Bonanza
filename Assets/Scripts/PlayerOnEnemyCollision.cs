463b62192d4e7d6b0c162d2516f784474d1c23c5
Muhammad Hassaan Moazan <boxloid0321321@gmail.com>
2024-08-05 00:40:38 +0500

    loose screen added with functionality

--- Assets/Scripts/PlayerOnEnemyCollision.cs
+++ Assets/Scripts/PlayerOnEnemyCollision.cs
@@ -1,33 +1,60 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public Animator StarAnimator;           // The Animator component
+    public Animator LevelSelectAnimator;
+    public Animator RetryAnimator;
+
+    public Button LevelSelect;
+    public Button Retry;
+    public Button Next;
+
+    private AudioSource audioSource;
+    public AudioClip awww;
+
+    void Start()
+    {
+        LevelSelect.gameObject.SetActive(false);
+        Retry.gameObject.SetActive(false);
+        Next.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision detected with: " + collision.gameObject.name); // Add this line
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Debug.Log("Collided with Enemy!"); // Add this line
-            RestartLevel();     //shall be replaced later
+            LoseScreen();
         }
     }
 
-    //this will be nuked later
-    private void RestartLevel()
+    private void LoseScreen()
     {
-        PersistentManager.Instance.IsRestarting = true;
-        StartCoroutine(LoadSceneAsync(SceneManager.GetActiveScene().name));
-    }
-
-    private IEnumerator LoadSceneAsync(string sceneName)
-    {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-
-        while (!asyncLoad.isDone)
+        audioSource.clip = awww;
+        if (AudioManager.Instance.play == true)
         {
-            yield return null;
+            AudioManager.Instance.audioSource.Pause();
+            StartCoroutine(PlayAgain());
+            audioSource.Play();
         }
+        StarAnimator.Play("StarsSpawn");
+
+        LevelSelect.gameObject.SetActive(true);
+        LevelSelectAnimator.Play("LevelSelectSpawn");
+
+        Retry.gameObject.SetActive(true);
+        RetryAnimator.Play("RetrySpawn");
+    }
+
+    private IEnumerator PlayAgain()
+    {
+        yield return new WaitForSeconds(4);
+        AudioManager.Instance.audioSource.Play();
     }
 }
