375bae24bb34379e640f1f609db1b62742e30d0f
Muhammad Hassaan Moazan <boxloid0321321@gmail.com>
2024-08-07 07:16:27 +0500

    stars, win and lose screen improved

--- Assets/Scripts/BananaControl.cs
+++ Assets/Scripts/BananaControl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class BananaControl : MonoBehaviour
 {
